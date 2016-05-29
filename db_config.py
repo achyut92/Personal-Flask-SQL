a191b6a19c43cca0b581263895549e2c4e875518
achyut <a0134598@u.nus.edu>
2016-05-29 21:45:13 +0800

    change db url

--- db_config.py
+++ db_config.py
@@ -1,2 +1,2 @@
-DATABASE = 'root:vaultdragon@104.155.192.96:3306/vault'
+DATABASE = 'root:vaultdragon@104.155.192.96:3306/vault?unix_socket=/cloudsql/vaultdragon-1326:asia-east1:vault-dragon-sql'
 DATABASEURI = 'mysql+mysqldb://' + DATABASE
